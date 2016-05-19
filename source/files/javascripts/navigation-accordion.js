cb763dd6f01fa64d5311ebae10badf8a8c66898c
Nick Fagerlund <nick.fagerlund@gmail.com>
2016-05-18 17:11:46 -0700

    WIP on revised nav JS using bootstrap's collapse.js
    This currently works! It doesn't handle the 'expand all' links yet, since
    I want to change their behavior anyway.

--- source/files/javascripts/navigation-accordion.js
+++ source/files/javascripts/navigation-accordion.js
@@ -1,74 +1,64 @@
+// This relies on Bootstrap's collapse.js.
 $( document ).ready( function() {
     "use strict";
-    var navList = $( "nav.main > ul" );
 
-    // Find nav links to current page
-        // To make the regex, we transform the current page's pathname to:
-        // - allow "/latest/" to also match any numerical version
-        // - handle "/" and "/index.html" identically.
-    var normalizedLocation = location.pathname.replace(/\/latest\//, "/(latest|[\\d\\.]+)/").replace(/(\/|\/index.html)$/, "/(index.html)?") + '$';
-    var locationTest = new RegExp(normalizedLocation);
-    var isLinkToCurrentPage = function(index, element) {
-        return locationTest.test( $( element ).prop( "href" ) );
-    };
-    var navLinksToCurrentPage = navList.find("a").filter( isLinkToCurrentPage );
+    // Nav list: A nav snippet for one particular lg. versioned document.
+    var navLists = $( "nav.main > ul" );
+    // Nav header: The title of a given nav list. Usually just the name of the document.
+    var navHeaders = $( "nav.main > h2, nav.main > h3" );
+    // Sub-list: A list within a nav list.
+    var navSubLists = navLists.find('ul');
+    // Nav section: An li element containing at least one sub-list and usually a title with <strong> markup.
+    var navSections = navLists.find('li:has(ul)');
 
-    var navSections = navList.find( "li:has(ul)" ); // an LI that contains a label followed by a list of contents
-    var navSectionLabels = navSections.children( "strong" );
-    var activeNavSections = navLinksToCurrentPage.parentsUntil(navList).filter(navSections); // can include parents of the parent section
+    // Find nav links to current page...
+            // To make the regex, we transform the current page's pathname to:
+            // - allow "/latest/" to also match any numerical version
+            // - handle "/" and "/index.html" identically.
+        var normalizedLocation = location.pathname.replace(/\/latest\//, "/(latest|[\\d\\.]+)/").replace(/(\/|\/index.html)$/, "/(index.html)?") + '$';
+        var locationRegexp = new RegExp(normalizedLocation);
+        var isLinkToCurrentPage = function(index, element) {
+            return locationRegexp.test( $( element ).prop( "href" ) );
+        };
+    var navLinksToCurrentPage = navLists.find("a").filter( isLinkToCurrentPage );
+
+    // Active nav sub-list: One that contains a link to the current page (directly, or in a sub-sub-list.)
+    var activeNavSections = navLinksToCurrentPage.parentsUntil(navLists).filter(navSections);
 
     // Disable links to current page
     navLinksToCurrentPage.addClass("disabled-nav-link");
     navLinksToCurrentPage.on("click", function(e) { e.preventDefault(); } );
 
-    // Add custom events to nav sections. Since we can have sections within sections
-    // with independent expansion state, we shouldn't let these events bubble up.
-    navSections.on( {
-        "toggleNavSection": function(e) {
+    var toggleSection = function(label, target) {
+        label.toggleClass("hidden-nav expanded-nav");
+        target.collapse('toggle');
+    };
+    var hideSection = function(label, target) {
+        label.addClass("hidden-nav").removeClass("expanded-nav");
+        target.collapse('hide');
+    };
+    var showSection = function(label, target) {
+        label.addClass("expanded-nav").removeClass("hidden-nav");
+        target.collapse('show');
+    };
+
+
+    // Allow nav sections to toggle their sublist if you click the ± sign or label
+    navSections.on("click", function(e) {
+        if (e.target === this || e.target === $(this).children('strong')[0]) {
             e.stopPropagation();
-            // Toggle the list item bullet
-            $( this ).toggleClass("hidden-nav expanded-nav");
-            // Slide the contents of the section
-            $( this ).children("ul").slideToggle( 200 );
-        },
-        "setExpanded": function(e) {
-            e.stopPropagation();
-            $( this ).addClass("expanded-nav").removeClass("hidden-nav");
-            $( this ).children("ul").css("display", "");
-        },
-        "setHidden": function(e) {
-            e.stopPropagation();
-            $( this ).addClass("hidden-nav").removeClass("expanded-nav");
-            $( this ).children("ul").css("display", "none");
+            toggleSection( $(this), $(this).children('ul') );
         }
     });
 
-    // Initialize all nav sections
-    navSections.trigger("setHidden");
-    // Expand any sections that contain the current page
-    activeNavSections.trigger("setExpanded");
+    // Mark sublists as collapsed.
+    navSubLists.addClass('collapse');
+    navSections.addClass("hidden-nav");
+    // Uncollapse the active ones.
+    activeNavSections.trigger('click');
 
-    // Clicking section labels will toggle their section
-    navSectionLabels.on("click", function() {
-        $( this ).parent("li").trigger("toggleNavSection");
-    });
+    // Mark top-level nav lists as collapsable.
+    navLists.addClass('collapse').addClass('in');
 
-    // Add a "toggle all" button if there is at least one navSection
-    if ( navSections.length > 0 ) {
-        navList.before( '<p style="text-align: center;"><a href="#" id="toggle-all-nav-sections">(↓ expand all ↓)</a></p>' );
-    }
-
-    // Enable the "toggle all" button
-    $( "a#toggle-all-nav-sections" ).on("click", function(e) {
-        e.preventDefault();
-        if ( $( this ).text() === '(↓ expand all ↓)' ) {
-            navSections.trigger("setExpanded");
-            $( this ).text('(↑ collapse all ↑)');
-        } else {
-            navSections.trigger("setHidden");
-            $( this ).text('(↓ expand all ↓)');
-        }
-    });
 
 });
-
